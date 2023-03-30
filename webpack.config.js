05ad309c590bef6c3e89abf0dd7c48782dd6a0a2
zc <syfhub@hotmail.com>
2023-03-30 11:01:39 +0800

    update

--- webpack.config.js
+++ webpack.config.js
@@ -4,25 +4,24 @@ const { Compilation } = require("webpack");
 
 
 const scriptPath = resolve(__dirname, "scripts");
-const mathPath = { mathJax: resolve(scriptPath, "mathjax") };
 /*******************************************************************************/
 
 const copyPlugin = new CopyPlugin({
     patterns: [
         {
-            context: "./node_modules/mathjax",
-            from: "LICENSE",
-            to: resolve(mathPath.mathJax, "LICENSE.txt"),
+            context: "./node_modules",
+            from: "mathjax",
+            to: resolve(scriptPath, "mathjax"),
         },
         {
-            context: "./node_modules/mathjax",
-            from: "README.md",
-            to: resolve(mathPath.mathJax, "README.md"),
+            context: "./node_modules",
+            from: "tippy.js",
+            to: resolve(scriptPath, "tippyjs"),
         },
         {
-            context: "./node_modules/mathjax",
-            from: "es5",
-            to: resolve(mathPath.mathJax, "es5"),
+            context: "./node_modules",
+            from: "@popperjs",
+            to: resolve(scriptPath, "popperjs"),
         },
     ]
 });
